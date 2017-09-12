72707596d98c8edaa8fc74cd1cac7efa6afee74c
Marius Maries <mariuscm@gmail.com>
2017-09-12 17:33:17 -0500

    Angular/Udemy first assignment submission

---
+++ src/app/warningAlert/warningAlert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-warningAlert',
+    // templateUrl: './warningAlert.component.html'
+    template: '<p class="bg-danger">Warning Alert Component</p>'
+})
+
+export class WarningAlertComponent {
+
+}
